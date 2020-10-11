5d2e977190bb13d43f5b2397ada0f35d712eaccb
Chenade <chenade0312@gmail.com>
2020-10-12 05:58:23 +0800

    Mission

---
+++ Assets/Mission/Bussard Ramjet/ammo_collect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ammo_collect : MonoBehaviour
+{
+    public string type;
+
+    private void Start()
+    {
+        Amno_System.amno = 0;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        switch (type)
+        {
+            case "h_11":
+                Amno_System.amno += 1;
+                break;
+
+            case "h_21":
+                Amno_System.amno += 3;
+                break;
+
+            case "h_31":
+                Amno_System.amno += 5;
+                break;
+
+            case "next_level":
+                SceneManager.LoadScene(19);
+                break;
+        }
+        Destroy(gameObject);
+    }
+}
